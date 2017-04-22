ee0874f31889c46cea1900b2bee58967ee5d7c85
zouqianli <1004248149@qq.com>
2017-04-22 18:20:29 +0800

    Initial Commit

---
+++ OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC
+//
+//  Created by 邹前立 on 2017/4/22.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
